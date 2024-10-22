f72d19e85f5c67398b358f0f3503b4b32a92211b
EzequielFerraris <ferraris.ezequiel.m@gmail.com>
2024-10-22 15:37:32 -0300

    primer commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { environment } from '../environmentConfig'; 
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment)), 
+    provideAuth(() => getAuth()), 
+    provideFirestore(() => getFirestore()), 
+    provideStorage(() => getStorage())],
+};
